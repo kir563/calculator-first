ea57098189acb3a3d4f24ad4f387896256a9cf41
kir563 <k563ke70@gmail.com>
2022-08-13 15:22:25 +0700

    First Commit

---
+++ calc-switch.js
@@ -0,0 +1,11 @@
+function calc(operation, a, b){
+    switch (operation) {
+        case 'add': return +a + +b;
+        case 'multi': return a * b;
+        case 'subtract': return a - b;
+    }
+}
+
+console.log(calc('add', 2, 3));
+console.log(calc('multi', 2, 3));
+console.log(calc('subtract', 2, 3));
