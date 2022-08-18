b7baf74976bc600f212610ec969b4b92cd732ac0
kir563 <k563ke70@gmail.com>
2022-08-18 22:03:43 +0700

    method object

--- calc-switch.js
+++ calc-switch.js
@@ -1,14 +1,63 @@
-function calc(operation, a, b){
-    switch (operation) {
-        case 'add': return +a + +b;
-        case 'multi': return a * b;
-        case 'subtract': return a - b;
+let operations = {
+    'add': '+',
+    'multi': '*',
+    'subtract': '-',
+    'deepClone': {
+        '1': 1,
+        '2': 2,
+    },
+    
+    sayHi() {
+        console.log(this['add']);
+    },
+}
+
+operations.sayHi();
+
+function calc(operation, a, b) {
+    
+    if(operation in this) {
+
+        switch (this[operation]) {
+            case '+':
+                console.log(a + b);
+                break
+            case '*':
+                console.log(a * b);
+                break
+            case '-':
+                console.log(a - b);
+                break
+        }
+    }else {
+        console.log('error');
     }
 }
-console.log(calc('add', 2, 3));
-console.log(calc('multi', 2, 3));
-console.log(calc('subtract', 2, 3));
+
+operations.calc = calc;
+
+operations.calc('add',3, 5);
+operations.calc('multi',3, 5);
+operations.calc('subtract',3, 5);
+
+console.log(operations);
+
+let cloneOperations = {};
+
+for (let operationsKey in operations) {
+    cloneOperations[operationsKey] = operations[operationsKey];
+    if (typeof operations[operationsKey] === 'object'){
+         operations[operationsKey] = Object.assign({}, operations[operationsKey])
+    }
+}
 
 
+operations['deepClone']['1'] = 11111;
 
-//Test create new branch on gitHub
+console.log(cloneOperations);
+
+cloneOperations['add'] = '*';
+
+cloneOperations.calc('add',3, 5);
+
+
